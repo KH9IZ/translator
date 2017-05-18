0ee4d2a21f0f5c3e7560b33c0973c8c8e028e63b
KH9IZ <knyazevegor01@mail.ru>
2017-05-19 00:13:06 +0500

    Added inline translate.
    Signed-off-by: KH9IZ <knyazevegor01@mail.ru>

--- translator.py
+++ translator.py
@@ -1,16 +1,64 @@
-import xml.dom.minidom
-import telebot
 import urllib
+import xml.dom.minidom
+
+import telebot
 from config import *
-import peewee
 
 bot = telebot.TeleBot(token)
 
 
+def translate(word):
+    text = word
+    lang = 'ru'
+    for i in text:
+        if ((i > 'a') and (i < 'z')) or ((i > 'A') and (i < 'Z')):
+            lang = 'ru'
+        else:
+            lang = 'en'
+        break
+    print(lang+" faweg")
+    args = {'key': key, 'text': text, 'lang': lang}
+    enc_args = urllib.parse.urlencode(args)
+    response = urllib.request.urlopen('https://translate.yandex.net/api/v1.5/tr/translate?' + enc_args)
+    xml_file = response.read()
+    xml_file = xml_file.decode('utf-8')
+    dom = xml.dom.minidom.parseString(xml_file)
+    dom.normalize()
+    text = dom.getElementsByTagName("text")[0]
+    return text.childNodes[0].nodeValue
+
+
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def query_answer(query):
+    text = query.query
+    lang = 'ru'
+    for i in text:
+        if ((i > 'a') and (i < 'z')) or ((i > 'A') and (i < 'Z')):
+            lang = 'ru'
+        else:
+            lang = 'en'
+        break
+    print(lang)
+    args = {'key': key, 'text': text, 'lang': lang}
+    enc_args = urllib.parse.urlencode(args)
+    response = urllib.request.urlopen('https://translate.yandex.net/api/v1.5/tr/translate?' + enc_args)
+    xml_file = response.read()
+    xml_file = xml_file.decode('utf-8')
+    dom = xml.dom.minidom.parseString(xml_file)
+    dom.normalize()
+    text = dom.getElementsByTagName("text")[0]
+    ans = telebot.types.InlineQueryResultArticle(
+        id='0',
+        title=text.childNodes[0].nodeValue,
+        input_message_content=telebot.types.InputTextMessageContent(text.childNodes[0].nodeValue)
+    )
+    bot.answer_inline_query(query.id, [ans])
+
+
 @bot.message_handler(content_types=['text'])
 def translate(message):
-    print(message)
     text = message.text
+    lang = 'ru'
     for i in text:
         if ((i > 'a') and (i < 'z')) or ((i > 'A') and (i < 'Z')):
             lang = 'ru'
@@ -32,4 +80,3 @@ def translate(message):
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
-db.close()
