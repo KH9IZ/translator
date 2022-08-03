5d57efd58a89107226ba09f4d3533d16e6d0a586
Knyazev Egor <knyazevegor01@mail.ru>
2022-08-03 13:27:48 +0300

    Tokens revoked

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-key = 'trnsl.1.1.20170503T205407Z.df3292ae8a21d767.fc3f096cd744f300221740b9ed1d82ff0e05402a'
-token = '303105597:AAHO9tqdPYb-wEzHUDXyOSEph39op1b8OfY'
+key = '<ya-trnslt-key>'
+token = '<tg-token>'
 lang = 'ru'
